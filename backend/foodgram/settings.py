4e85f10874803bdddd14a338167b392c3fb17d54
Данила <danilalogunov13@gmail.com>
2021-07-21 12:27:25 +0300

    deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -55,7 +55,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
@@ -63,13 +63,6 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
-    }
-}"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
